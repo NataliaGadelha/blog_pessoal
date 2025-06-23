100fcb6917d5b9b042dad6cb1b4190e17e97624e
NataliaGadelha <natt.gdl25@gmail.com>
2025-06-23 11:46:38 -0300

    Adicionando os métodos GetAll e FindAll e conectando ao DB

---
+++ src/postagem/controllers/postagem.controller.ts
@@ -0,0 +1,14 @@
+import { PostagemService } from '../services/postagem.service';
+import { Postagem } from './../entities/postagem.entity';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+
+@Controller('/postagens')
+export class PostagemController {
+  constructor(private readonly postagemService: PostagemService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Postagem[]> {
+    return this.postagemService.findAll();
+  }
+}
